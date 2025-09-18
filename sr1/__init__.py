93a1ccae42e3581b5359e0668b8ac500100185ae
VestaxUA <valiktkachishen@gmail.com>
2025-09-18 16:44:40 +0300

    створення функції аналізу інформації про регістр у введенному рядку

--- sr1/__init__.py
+++ sr1/__init__.py
@@ -4,3 +4,14 @@ def print_string(s: str) -> None:
         return
     print(s)
 
+
+def analyze_string(s: str) -> None:
+    if not isinstance(s, str):
+        print("Помилка: аргумент має бути рядком!")
+        return
+    if s.isupper():
+        print("Усі літери у верхньому регістрі.")
+    elif s.islower():
+        print("Усі літери у нижньому регістрі.")
+    else:
+        print("Рядок містить як великі, так і малі літери.")
