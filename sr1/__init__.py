3302df6377ca19eb22a00b557659a8a8d1902494
VestaxUA <valiktkachishen@gmail.com>
2025-09-18 16:51:50 +0300

    створення модулю для генератора

--- sr1/__init__.py
+++ sr1/__init__.py
@@ -1,10 +1,2 @@
 
 
-def even_odd_generator():
-    toggle = True
-    while True:
-        if toggle:
-            yield "Парне"
-        else:
-            yield "Непарне"
-        toggle = not toggle
