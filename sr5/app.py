6b12acb7ff2cdc5dbfefc8b16d182c185d441159
VestaxUA <valiktkachishen@gmail.com>
2025-11-07 01:12:21 +0200

    додавання маршрутів /error і /status в app.py

--- sr5/app.py
+++ sr5/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify
 import logging
+import time
 
 app = Flask(__name__)
 
@@ -9,10 +10,34 @@ logging.basicConfig(
     format='%(asctime)s [%(levelname)s] %(message)s'
 )
 
+start_time = time.time()
+request_count = 0
+
+@app.before_request
+def before_request():
+    global request_count
+    request_count += 1
+
 @app.route('/')
 def home():
     logging.info("Отримано запит до '/'")
     return "Сервіс працює"
+
+@app.route('/error')
+def error():
+    logging.warning("Виклик ендпоїнту /error — буде помилка")
+    x = 1 / 0
+    return str(x)
+
+@app.route('/status')
+def status():
+    uptime = round(time.time() - start_time, 2)
+    info = {
+        "uptime_seconds": uptime,
+        "request_count": request_count
+    }
+    logging.info("Надано статус застосунку")
+    return jsonify(info)
 
 if __name__ == '__main__':
     app.run(debug=True)
