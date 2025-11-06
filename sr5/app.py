b07971c2b2a314dd23b86342a865939e2acceea9
VestaxUA <valiktkachishen@gmail.com>
2025-11-07 01:07:29 +0200

    налаштування логування в app.py

--- sr5/app.py
+++ sr5/app.py
@@ -1,9 +1,17 @@
 from flask import Flask
+import logging
 
 app = Flask(__name__)
 
+logging.basicConfig(
+    filename='app.log',
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s'
+)
+
 @app.route('/')
 def home():
+    logging.info("Отримано запит до '/'")
     return "Сервіс працює"
 
 if __name__ == '__main__':
